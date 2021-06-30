2699b797e5d1427a19352c568876314033f0b3b0
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-06-30 21:59:38 +0530

    Create lycia.js

---
+++ lycia.js
@@ -0,0 +1,9 @@
+const fetch = require("node-fetch")
+
+class Lyciabot {
+
+  constructor(options = { name: "Lyciabot" }) {
+    this.name = options.name ? options.name : "Lyciabot"
+  }
+
+#please wait sir 😈😈😈
